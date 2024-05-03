a4e14b7441785a5197e8d2d393386bb61bc0bf88
AlexHerbertGit <alex.herbert.tech@gmail.com>
2024-05-03 14:11:03 +1200

    Finished the user model with hashing and admin assign functions for username and password

--- models/user.js
+++ models/user.js
@@ -23,3 +23,25 @@ user.statics.create = function(username, password) {
     return user.save()
 };
 
+//Find user by using the username
+User.statics.findOneByUsername = function(username) {
+    return this.findOne({
+        username
+    }).exec()
+};
+
+//Verify the password of the user document
+User.methods.verify = function(password) {
+    const encrypted = crypto.createHmac('sha1', config.secret)
+                    .update(password)
+                    .digest('base64')
+    console.log(this.password === encrypted)
+    return this.password === encrypted
+};
+
+User.methods.assignAdmin = function() {
+    this.admin = true
+    return this.save()
+};
+
+module.exports = mongoose.model('User', User)
